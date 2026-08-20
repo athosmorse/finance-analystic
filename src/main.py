e68cb8df68db8d121fa2ac96762e23771fcd1fad
athosmorse <athoscrat@outlook.com>
2026-08-20 17:00:15 -0300

    iniciando a captura do valor da PE

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 import requests
 
-response = requests.get("https://brapi.dev/api/quote/PETR4")
+response_PETR4 = requests.get("https://brapi.dev/api/quote/PETR4") # Chamada à API para obter informações sobre a ação PETR4
 
-print(response.status_code)
+dados_PETR4 = response_PETR4.json() # Converte a resposta em formato JSON
+
+print("Informações sobre a ação PETR4:")
+print(dados_PETR4)
+
+valor_acao_PETR4 = dados_PETR4['results'][0]['regularMarketPrice']
+
+print("====================================================================================================")
+
+print(f"Valor atual da ação PETR4: R$ {valor_acao_PETR4}")
