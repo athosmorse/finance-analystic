62cc269f14e5aa5180329418a485719cb94a9302
athosmorse <athoscrat@outlook.com>
2026-08-25 08:50:33 -0300

    4° alteração: aplicando lógica de busca do PE automática

--- src/coletor.py
+++ src/coletor.py
@@ -4,7 +4,6 @@ def buscar_acao(simbolo):
     response = requests.get(f"https://brapi.dev/api/quote/{simbolo}")
     dados = response.json() # Converte a resposta em formato JSON
     valor_acao = dados['results'][0]['regularMarketPrice']
-    
     pe = dados['results'][0]['priceEarnings']
     
     return valor_acao, pe
--- src/main.py
+++ src/main.py
@@ -5,5 +5,14 @@ resultados = {}
 for ativo in ['PETR4', 'VALE3', 'ITUB4']:
     valor_acao, pe = buscar_acao(ativo)
     resultados[ativo] = {'preco': valor_acao, 'pe': pe}
-
 print("Resultados finais:", resultados)
+
+melhor_ativo = 'PETR4'
+melhor_pe = resultados['PETR4']['pe']
+
+for ativo, dados in resultados.items():
+    if dados['pe'] < melhor_pe:
+        melhor_pe = dados['pe']
+        melhor_ativo = ativo
+        
+print(f'Melhor ativo: {melhor_ativo} com o P/E de {melhor_pe:.2f}')
