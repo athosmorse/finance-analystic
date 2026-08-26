3a7c6b95bade34d8268811a284940b1b55246bb4
athosmorse <athoscrat@outlook.com>
2026-08-26 10:31:04 -0300

    criando e fazendo a lógica da analise da amplitude percentual do ativo

--- src/analise.py
+++ src/analise.py
@@ -1,7 +1,7 @@
 from coletor import buscar_acao
 
 def analisar_acao(simbolo):
-    _, _,valor_2WH, valor_2WL = buscar_acao(simbolo)
+    _, _, valor_2WH, valor_2WL = buscar_acao(simbolo)
     amplitude = ((valor_2WH - valor_2WL) / valor_2WL) * 100 
     # Calcula a amplitude percentual do ativo
     
