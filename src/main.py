2e523d66d23311a7222c6b064156ed13dcb444ab
athosmorse <athoscrat@outlook.com>
2026-08-26 16:02:29 -0300

    finalizando a importação da amplitude dentro do main

--- src/main.py
+++ src/main.py
@@ -4,9 +4,13 @@ from analise import analisar_acao
 resultados = {} #Biblioteca para armazenar os resultados das ações
 
 for ativo in ['PETR4', 'VALE3', 'ITUB4']:
-    valor_acao, pe = buscar_acao(ativo)
-    resultados[ativo] = {'preco': valor_acao, 'pe': pe}
+    valor_acao, pe, valor_2WH, valor_2WL = buscar_acao(ativo)
+    amplitude = analisar_acao(valor_2WH, valor_2WL)
+    resultados[ativo] = {'preco': valor_acao, 'pe': pe, 'amplitude': amplitude}
 print("Resultados finais:", resultados)
+
+for ativo, dados in resultados.items():
+    print(f"{ativo} - Amplitude: {dados['amplitude']:.2f}%")
 
 # Aqui, defini de forma estática o melhor ativo como PETR4
 
@@ -20,5 +24,3 @@ for ativo, dados in resultados.items():
         melhor_ativo = ativo
 # Se o código encontrar um ativo com P/E menor que o melhor P/E atual, ele atualiza o melhor P/E e o melhor ativo.
 print(f'Melhor ativo: {melhor_ativo} com o P/E de {melhor_pe:.2f}')
-
-
