a2d43709e74465c9dad5545ff5c59983a6d3fd39
athosmorse <athoscrat@outlook.com>
2026-08-28 17:13:07 -0300

    ultima alteração na análise

--- src/analise.py
+++ src/analise.py
@@ -1,5 +1,3 @@
-from coletor import buscar_acao
-
 def analisar_acao(valor_2WH, valor_2WL):
     amplitude = ((valor_2WH - valor_2WL) / valor_2WL) * 100 
     # Calcula a amplitude percentual do ativo
