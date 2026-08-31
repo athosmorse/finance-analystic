cded33e21022f5f9c123c1c821e9a3d34260f2f5
athosmorse <athoscrat@outlook.com>
2026-08-31 18:07:33 -0300

    adicionando a lógica de SCORE para verificar qual é o melhor ativo

--- src/main.py
+++ src/main.py
@@ -12,15 +12,39 @@ print("Resultados finais:", resultados)
 for ativo, dados in resultados.items():
     print(f"{ativo} - Amplitude: {dados['amplitude']:.2f}%")
 
-# Aqui, defini de forma estática o melhor ativo como PETR4
+# Neste momento irei pegar o segundo elemento da tupla que criei no FOR em cima
+# Estou extraindo o valor que o sorted vai me retornar, e vou comparar com o valor do P/E que denifi como melhor.
+
+# Criando o campo 'score' no JSON zerado
+for ativo in resultados:
+    resultados[ativo]['score'] = 0
+
+
+# Criando o Ranking P/E - soma dos pontos no score
+# A lógica aqui consiste em pegar o elemento da tupla, e retornar o valor para comparação ⬇
+ranking_pe = sorted(resultados.items(), key=lambda item: item[1]['pe'])
+for posicao, (ativo, dados) in enumerate(ranking_pe):
+    # posicao vale 0, 1, 3... ativo vale 'PETR4', 'ITUB4'... e dados vale o dicionário interno
+    pontos = len(ranking_pe) - posicao
+    resultados[ativo]['score'] += pontos
+
+# Criando o Ranking Amplitude - soma dos pontos no score
+ranking_amplitude = sorted(resultados.items(), key=lambda item: item[1]['amplitude'])
+for posicao, (ativo, dados) in enumerate(ranking_amplitude):
+    pontos = len(ranking_amplitude) - posicao
+    resultados[ativo]['score'] += pontos
 
 melhor_ativo = 'PETR4'
-melhor_pe = resultados['PETR4']['pe']
+melhor_score = resultados['PETR4']['score']
 
 for ativo, dados in resultados.items():
-# Essa lógica ira servir para comparar o P/E de cada ativo com o melhor P/E que definimos.
-    if dados['pe'] < melhor_pe:
-        melhor_pe = dados['pe']
+# Essa lógica ira servir para comparar o socre de cada ativo com o melhorscore que definimos.
+    if dados['score'] > melhor_score:
+        melhor_score = dados['score']
         melhor_ativo = ativo
-# Se o código encontrar um ativo com P/E menor que o melhor P/E atual, ele atualiza o melhor P/E e o melhor ativo.
-print(f'Melhor ativo: {melhor_ativo} com o P/E de {melhor_pe:.2f}')
+   
+print(f'Melhor ativo: {melhor_ativo} com o Socre de {melhor_score:.2f}')    
+    
+print("=======================================================================================================================")
+print(resultados)
+    
